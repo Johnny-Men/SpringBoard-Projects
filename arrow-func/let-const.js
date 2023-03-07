a402f1049b3e655d8512912271bed7a7ec030ee5
Johnny <john444444@outlook.com>
2023-03-07 15:25:22 -0500

    let and const practice

---
+++ arrow-func/let-const.js
@@ -0,0 +1,13 @@
+const PI = 3.14;
+
+// var can be reassigned and redeclared in any scope except a function.
+// let can be reassigned and is only defined in the scopeit was declared in.
+
+// // const can't be redeclared or reassigned and only defined in scope it was declared in.
+// var can be reassigned and redeclared in any scope except a function.
+
+// let can be reassigned const can not. Both can't be redeclared.
+
+// When a js file is being loaded with variables being declared by var,
+// the variables are declared but value is undefined. Js knows about the
+// variable before the variable is declared.
